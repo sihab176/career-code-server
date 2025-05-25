4122a9af22077e7a2113c0e4ad875d5b2eac4808
ProgrammingHero1 <programming.hero1@gmail.com>
2025-05-25 08:25:57 +0200

    added application related get and post api

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     await client.connect();
 
     const jobsCollection = client.db('careerCode').collection('jobs');
+    const applicationsCollection = client.db('careerCode').collection('applications')
 
     // jobs api
     app.get('/jobs', async (req, res) => {
@@ -38,10 +39,40 @@ async function run() {
 
     app.get('/jobs/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await jobsCollection.findOne(query);
       res.send(result)
+    });
+
+    // job applications related apis
+    app.get('/applications', async (req, res) => {
+      const email = req.query.email;
+
+      const query = {
+        applicant: email
+      }
+      const result = await applicationsCollection.find(query).toArray();
+
+      // bad way to aggregate data
+      for (const application of result) {
+        const jobId = application.jobId;
+        const jobQuery = { _id: new ObjectId(jobId) }
+        const job = await jobsCollection.findOne(jobQuery);
+        application.company = job.company
+        application.title = job.title
+        application.company_logo = job.company_logo
+      }
+
+      res.send(result);
     })
+
+    app.post('/applications', async (req, res) => {
+      const application = req.body;
+      console.log(application);
+      const result = await applicationsCollection.insertOne(application);
+      res.send(result);
+    });
+
 
 
 
