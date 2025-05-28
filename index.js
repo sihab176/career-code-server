b5ba666e98d7d443b01cfb7ed5cee6f4cb789cf0
Programming Hero <programming.hero1@gmail.com>
2025-05-27 22:56:28 -0500

    get application count for each job

--- index.js
+++ index.js
@@ -52,6 +52,22 @@ async function run() {
     //   res.send(result);
     // })
 
+    app.get('/jobs/applications', async (req, res) => {
+      const email = req.query.email;
+      const query = { hr_email: email };
+      const jobs = await jobsCollection.find(query).toArray();
+
+      // should use aggregate to have optimum data fetching
+      for (const job of jobs) {
+        const applicationQuery = { jobId: job._id.toString() }
+        const application_count = await applicationsCollection.countDocuments(applicationQuery)
+        job.application_count = application_count;
+      }
+      res.send(jobs);
+
+    })
+
+
     app.get('/jobs/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
@@ -66,6 +82,7 @@ async function run() {
       res.send(result);
     })
 
+    
     // job applications related apis
     app.get('/applications', async (req, res) => {
       const email = req.query.email;
@@ -104,9 +121,9 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/applications/:id', async(req, res) =>{
+    app.patch('/applications/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
         $set: {
           status: req.body.status
